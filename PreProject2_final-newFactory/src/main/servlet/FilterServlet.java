7238b11222d8077d595563655fec3ec8494d9e63
Alexander-Bolotov <37707228+Alexander-Bolotov@users.noreply.github.com>
2020-01-08 21:36:03 +0400

    commit1

--- PreProject2_final-newFactory/src/main/servlet/FilterServlet.java
+++ PreProject2_final-newFactory/src/main/servlet/FilterServlet.java
@@ -6,6 +6,8 @@ import src.main.service.Service;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 //@WebServlet("/filter")
@@ -18,29 +20,26 @@ public class FilterServlet implements Filter {
     public void init(FilterConfig filterConfig) throws ServletException {
         this.filterConfig = filterConfig;
         String act = filterConfig.getInitParameter("active");
-        if(act != null){
+        if (act != null) {
             active = (act.toUpperCase().equals("TRUE"));
         }
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         Service service = Service.getInstance();
-        if (active){
-            String name = servletRequest.getParameter("name");
-            String password = servletRequest.getParameter("password");
-            User user = new User(name, password);
-            String role = service.getRoleByUser(user);
 
-            if(role==null){
-                System.out.println("role = null!");
-            } else if(role.equals("Admin")){
-                servletRequest.getRequestDispatcher("admin.jsp").forward(servletRequest, servletResponse);
-            } else if(role.equals("User")){
-                servletRequest.getRequestDispatcher("user.jsp").forward(servletRequest, servletResponse);
-            } else {
-                System.out.println("Какая-то фигня!");
-            }
+        String name = httpServletRequest.getParameter("name");
+        String password = httpServletRequest.getParameter("password");
+        User user = new User(name, password);
+
+        String role = service.getRoleByName(name);
+
+        if (role.equals("Admin")) {
+            servletRequest.getRequestDispatcher("admin.jsp").forward(servletRequest, servletResponse);
+        } else if(role.equals("User")){
+            servletRequest.getRequestDispatcher("user.jsp").forward(servletRequest, servletResponse);
         }
     }
 
